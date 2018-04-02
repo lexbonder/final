e076147621c3b4d40227f9edbaa0762398ebd53a
Alex Bonder <lexbonder@gmail.com>
2018-04-02 10:40:27 -0600

    Render seeds

--- public/scripts.js
+++ public/scripts.js
@@ -15,4 +15,29 @@ const addItem = event => {
   $('.input').reset()
 }
 
+const renderItems = marsItems => {
+  marsItems.forEach( marsItem => {
+    let { itemName, packed } = marsItem;
+    let prePacked = packed ? 'checked' : ''
+    $('.item-container').prepend(`
+    <article>
+      <div class="top-of-card">
+        <h2>${itemName}</h2>
+        <button>Delete</button>
+      </div>
+      <input type="checkbox" id="checkbox" ${prePacked}>
+      <label for="check">Packed</label>
+    </article>
+  `)
+  })
+}
+
+const getItemList = async () => {
+  console.log('yo')
+  const initialFetch = await fetch('http://localhost:3000/api/v1/marsItems')
+  const marsItems = await initialFetch.json();
+  renderItems(marsItems);
+}
+
 $('.submit').on('click', addItem)
+$('document').ready(getItemList)
